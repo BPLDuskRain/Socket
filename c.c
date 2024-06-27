475e31b373342ac2bdc5ff124b814f0c4788b98a
BPLDuskRain <3108529508@qq.com>
2024-06-28 02:19:05 +0800

    2:no sigint version

--- c.c
+++ c.c
@@ -1,43 +1,176 @@
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
+# include <stdint.h>
+# include <string.h>
 # include <sys/types.h>
 # include <sys/socket.h>
 # include <netinet/in.h>
-int main(){
+# include <arpa/inet.h>
+# include <errno.h>
+
+int exitFlag;
+
+struct RQTPDU{
+    char pdu[20];
+};
+struct RQTPDU_in{
+    uint32_t op;
+    int64_t op1;
+    int64_t op2;
+};
+
+struct REPPDU{
+    char pdu[8];
+};
+struct REPPDU_in{
+    int64_t res;
+};
+
+uint64_t htonll(uint64_t value){
+    return (((uint64_t)htonl(value)) << 32) + htonl(value >> 32);
+}
+uint64_t ntohll(uint64_t value){
+    return (((uint64_t)ntohl(value)) << 32) + ntohl(value >> 32);
+}
+
+uint32_t opRead(char* opchar){
+    if(exitFlag == 1) return 0;
+    int32_t op;
+    char opString[5];
+    scanf("%s", opString);
+    getchar();
+    if(strcmp(opString, "ADD") == 0){
+        *opchar = '+';
+        op = 0x00000001;
+    }
+    else if(strcmp(opString, "SUB") == 0){
+        *opchar = '-';
+        op = 0x00000002;
+    }
+    else if(strcmp(opString, "MUL") == 0){
+        *opchar = '*';
+        op = 0x00000004;
+    }
+    else if(strcmp(opString, "DIV") == 0){
+        *opchar = '/';
+        op = 0x00000008;
+    }
+    else if(strcmp(opString, "MOD") == 0){
+        *opchar = '%';
+        op = 0x00000010;
+    }
+    else if(strcmp(opString, "EXIT") == 0){
+        exitFlag = 1;
+        return 0;
+    }
+    else{
+        printf("opRead failed\n");
+        return -1;
+    }
+    return op;
+}
+int64_t numRead(){
+    if(exitFlag == 1) return 0;
+    int64_t op;
+    char opString[10];
+    char* end;
+    scanf("%s", opString);
+    getchar();
+    if(strcmp(opString, "EXIT") == 0){
+        exitFlag = 1;
+        return 0;
+    }
+    else{
+        op = (int64_t)strtol(opString, &end, 10);
+        if(*end != '\0'){
+            printf("invalid input\n");
+            return -1;
+        }
+        return op;
+    }
+    
+}
+
+int cli_biz(int fd){
+    struct RQTPDU_in sendbuf;
+    
+    while(1){
+        //receive client input
+        int32_t op;
+        int64_t op1;
+        int64_t op2;
+        char opchar;
+        while(1){
+            exitFlag = 0;
+            
+            op = opRead(&opchar);
+            if (op == -1) continue;
+            op1 = numRead();
+            if (op1 == -1) continue;
+            op2 = numRead();
+            if (op2 == -1) continue;
+
+            if(exitFlag == 1){
+                printf("[cli] command EXIT received\n");
+                return 0;
+            }
+            break;
+        }
+        sendbuf.op = htonl(op);
+        sendbuf.op1 = (int64_t)htonll(op1);
+        sendbuf.op2 = (int64_t)htonll(op2);
+
+        //printf("%d,%ld %ld was sent\n",sendbuf.op,sendbuf.op1,sendbuf.op2);
+
+        int sendmsg = write(fd, (struct RQTPDU*)&sendbuf, sizeof(sendbuf));
+            if(sendmsg < 0){
+                perror("send failed\n");
+                return -1;
+                
+            }
+            else if(sendmsg == 0){
+                perror("disconnected\n");
+                break;
+            }
+            else{
+                struct REPPDU_in recvbuf;
+                read(fd, (struct REPPDU*)&recvbuf, sizeof(struct REPPDU));
+                //printf("client recv: %ld\n", recvbuf.res);
+                recvbuf.res = (int64_t)ntohll(recvbuf.res);
+                //printf("client recv: %ld\n", recvbuf.res);
+                printf("[rep_rcv] %ld %c %ld = %ld\n", op1, opchar, op2, recvbuf.res);
+            }
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[]){
     //create socket
     int fd = socket(AF_INET, SOCK_STREAM, 0);
 
     //create sockaddr
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(22222);
-    inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr.s_addr);
-
-    //create buf
-    char sendbuf[] = "Hello,world\n";
-    char recvbuf[100];
+    addr.sin_port = htons((uint16_t)atoi(argv[2]));
+    inet_pton(AF_INET, argv[1], &addr.sin_addr.s_addr);
 
     //connecting
-    if(connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == 0){
-        int msglen = send(fd, &sendbuf, sizeof(sendbuf), 0);
-        if(msglen > 0){
-            recv(fd, &recvbuf, sizeof(recvbuf), 0);
-            printf("%s", recvbuf);
-        }
-        else if(msglen < 0){
-            perror("send failed\n");
+    int connecter = connect(fd, (struct sockaddr*)&addr, sizeof(addr));
+        if(connecter == -1){
+            perror("connect failed\n");
             return -1;
         }
-        else{
-            perror("disconnected\n");
-        }    
-    }
-    else{
-        //error
-        perror("connect failed\n");
+    printf("[cli] server[%s:%s] is connected!\n", argv[1], argv[2]);
+    
+    //cli_biz
+    int flag = cli_biz(fd);
+    if(flag == 0){
+        close(fd);
+        printf("[cli] connfd is closed!\n");
+        printf("[cli] client is going to exit!\n");
+        return 0;
+    }else{
         return -1;
     }
-
-    close(fd);
-    return 0;
 }
--- s.c
+++ s.c
@@ -1,12 +1,66 @@
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
+# include <stdint.h>
 # include <sys/types.h>
 # include <sys/socket.h>
 # include <netinet/in.h>
+# include <arpa/inet.h>
+# include <errno.h>
 
 # define LISTEN_NUM 1
 
-int main(){
+int sigint_flag = 0;
+
+struct RQTPDU{
+    char pdu[20];
+};
+struct RQTPDU_in{
+    uint32_t op;
+    int64_t op1;
+    int64_t op2;
+};
+
+struct REPPDU{
+    char pdu[8];
+};
+struct REPPDU_in{
+    int64_t res;
+};
+
+uint64_t htonll(uint64_t value){
+    return (((uint64_t)htonl(value)) << 32) + htonl(value >> 32);
+}
+uint64_t ntohll(uint64_t value){
+    return (((uint64_t)ntohl(value)) << 32) + ntohl(value >> 32);
+}
+
+int64_t compute(uint32_t op, int64_t op1, int64_t op2, char* opchar){
+    switch(op){
+        case 0x00000001:
+            *opchar = '+';
+            return op1+op2;
+        case 0x00000002:
+            *opchar = '-';
+            return op1-op2;
+        case 0x00000004:
+            *opchar = '*';
+            return op1*op2;
+        case 0x00000008:
+            *opchar = '/';
+            return op1/op2;
+        case 0x00000010:
+            *opchar = '%';
+            return op1%op2;
+    }
+}
+
+void handle_sigint(int sig){
+    printf("[srv] SIGINT is coming!\n");
+    sigint_flag = 1;
+}
+
+int main(int argc, char* argv[]){
     //create socket
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if(fd == -1) {
@@ -17,44 +71,75 @@ int main(){
     //create sockaddr
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(22222);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons((uint16_t)atoi(argv[2]));//port from terminal to inet
+    int pton = inet_pton(AF_INET, argv[1], &(addr.sin_addr.s_addr));//ip from to inet
+        if(pton == 0){
+            perror("invalid chars\n");
+        }else if(pton == -1){
+            perror("pton failed\n");
+        }
 
     //bind
-    if(bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1){
+    int binder = bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+    if(binder == -1){
         perror("socket bind failed\n");
         return -1;
     }
     
     //listen
-    if(listen(fd, LISTEN_NUM) == -1){
+    int listenner = listen(fd, LISTEN_NUM);
+    if(listenner == -1){
         perror("start listen failed\n");
         return -1;
     }
+    printf("[srv] server[%s:%s] is initializing!\n",argv[1],argv[2]);
+
+    //sigint
+    
 
     while(1){
         struct sockaddr_in caddr;
         int caddrlen = sizeof caddr;
         //accept connection
         int cfd = accept(fd, (struct sockaddr*)&caddr, &caddrlen);
-        if(cfd == -1){
-            perror("accept connection failed\n");
-            return -1;
-        }
+            if(cfd == -1){
+                if(errno == EINTR){
+                    continue;
+                }
+                else{
+                    perror("accept connection failed\n");
+                    return -1;
+                }
+            }
+        char *cip = inet_ntoa(caddr.sin_addr);
+        short cport = ntohs(caddr.sin_port);
+        printf("[srv] client[%s:%hu] is accepted!\n", cip, cport);
+
         while(1){
-            char buf[1000];
-            int msglen = recv(cfd, buf, sizeof(buf), 0);
-            if(msglen < 0){
+            struct RQTPDU_in recvbuf;
+            struct REPPDU_in sendbuf;
+            int recvmsg = read(cfd, &recvbuf, sizeof(recvbuf));
+            if(recvmsg < 0){
                 perror("receive failed\n");
                 return -1;
             }
-            else if(msglen > 0){
-                printf("%s\n",buf);
-                send(cfd, buf, msglen, 0);
-            }
-            else {
+            else if(recvmsg ==0){
                 printf("disconnected\n");
                 break;
+            }
+            else {
+                recvbuf.op = ntohl(recvbuf.op);
+                recvbuf.op1 = (int64_t)ntohll(recvbuf.op1);
+                recvbuf.op2 = (int64_t)ntohll(recvbuf.op2);
+                
+                //printf("server recv: %d, %ld %ld\n",recvbuf.op, recvbuf.op1, recvbuf.op2);
+                char opchar;
+                sendbuf.res = compute(recvbuf.op, recvbuf.op1, recvbuf.op2, &opchar);
+                printf("[rqt_res] %ld %c %ld = %ld\n", recvbuf.op1, opchar, recvbuf.op2, sendbuf.res);
+                //printf("server send: %ld\n",sendbuf.res);
+                sendbuf.res = (int64_t)htonll(sendbuf.res);
+                //printf("server send: %ld\n",sendbuf.res);
+                write(cfd, &sendbuf, sizeof(sendbuf));
             }
         }
         close(cfd);
